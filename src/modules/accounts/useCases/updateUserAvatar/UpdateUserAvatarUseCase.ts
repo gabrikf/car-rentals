9050a6cae6a99973a3f006b5d00a3cc6f9bb509d
Gabriel Koch Fodi <gabrielkochfodi@gmail.com>
2022-04-06 16:57:14 -0300

    images upload ok

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { removeFileFromPaths } from '../../../../utils/removeFileFromPaths';
 import { IUpdateUserAvatarDTO } from '../../dtos/IUpdateUserAvarDTO';
 import { IUserRepository } from '../../repositories/IUserRepository';
 
@@ -12,7 +13,9 @@ export class UpdateUserAvatarUseCase {
 
   async execute({ user_id, avatar_file }: IUpdateUserAvatarDTO): Promise<void> {
     const user = await this.userRepository.findById(user_id);
-
+    if (user.avatar) {
+      await removeFileFromPaths(`./tmp/avatar/${user.avatar}`);
+    }
     user.avatar = avatar_file;
 
     await this.userRepository.create(user);
